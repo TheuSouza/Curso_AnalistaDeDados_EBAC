4e95b4d790627eaaf74de3fe634931527d7bd97c
TheuSouza <iam.mths.s@gmail.com>
2024-11-18 17:07:51 -0300

    :heavy_plus_sign: Adicionando Módulo 20

--- Outros/Requests_modulo.py
+++ Outros/Requests_modulo.py
@@ -1,15 +1,27 @@
 import requests
-from requests.exceptions import HTTPError
+from requests.exceptions import Timeout, RequestException, HTTPError
 
 conteudo = None
 URL = 'http://www.google.com'
 
-# Modelo padrão para fazer busca de site usando requests
 try:
     resposta = requests.get(URL)
     resposta.raise_for_status()
-except HTTPError as exc:
-    print(exc)
+
+except HTTPError as err_http:
+    print(f'Erro de HTTP: {err_http}')
+
+except ConnectionError as err_conn:
+    print(f'Erro de conexão: {err_conn}')
+
+except Timeout as err_timeout:
+    print(f'Tempo de resposta excedido: {err_timeout}')
+
+except RequestException as err_req:
+    print(f'Erro na requisição: {err_req}')
+
+except Exception as exc:
+    print(f'Ocorreu um erro inesperado: {exc}')
+
 else:
     conteudo = resposta.text
-
